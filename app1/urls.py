501887e0a7351cede98b4c384ba477c7a28de1eb
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2023-11-07 02:26:28 +0530

    some changes in the post view

--- app1/urls.py
+++ app1/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.post,name="home" ),
     path('view',views.view_post,name='view_post'),
     path('delete/<int:id>',views.delete_post,name='delete'),
+
     path('edit/<int:id>',views.edit_post,name='edit'),
     
     
