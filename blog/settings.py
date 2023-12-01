07fb6f06891cf816d228f3aaecda32418b0a73ab
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2023-12-01 22:51:28 +0530

    mediaroot issue fixed

--- blog/settings.py
+++ blog/settings.py
@@ -135,19 +135,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-MEDIA_ROOT = BASE_DIR/'uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
 
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static/'
 
 
-# CSRF_TRUSTED_ORIGINS=['*']
+CSRF_TRUSTED_ORIGINS=['*']
 
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
